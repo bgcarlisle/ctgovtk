dd4eaa15471ebc00c10cf2b998fce42bc15907ae
The research fairy <bgcarlisle@users.noreply.github.com>
2024-12-17 15:12:00 -0500

    Added function for downloading data based on NCT Number search

---
+++ R/ctgovtk-package.R
@@ -0,0 +1,14 @@
+#' @details This package provides various functions for interacting
+#'     with ClinicalTrials.gov
+#'
+#' @details ctgov_nct_search() returns a set of trial data from
+#'     ClinicalTrials.gov based on a search of NCT Numbers
+#'
+#' @references Carlisle, BG. The grey literature, 2024.
+#'
+#' #' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
