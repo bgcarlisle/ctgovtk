7fa58270fac6a866e14bd888dc02c3daa5960eb9
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2024-12-18 08:32:18 -0500

    Rename variable for clarity

--- tests/testthat/test-ctgov_ncts.R
+++ tests/testthat/test-ctgov_ncts.R
@@ -1,7 +1,7 @@
 test_that("NCT search works", {
-    json <- ctgov_ncts("NCT03430843")
+    response <- ctgov_ncts("NCT03430843")
     expect_equal(
-        json$studies[[1]]$protocolSection$identificationModule$officialTitle,
+        response$studies[[1]]$protocolSection$identificationModule$officialTitle,
         "A Randomized, Controlled, Open-label, Global Phase 3 Study Comparing the Efficacy of the Anti-PD-1 Antibody Tislelizumab (BGB-A317) Versus Chemotherapy as Second Line Treatment in Patients With Advanced Unresectable/Metastatic Esophageal Squamous Cell Carcinoma"
     )
 })
