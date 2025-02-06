217b382d674f7a338a499acfbf7f3c30f61de138
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2025-02-06 11:30:37 -0500

    Renamed function to plural

--- R/extract_drug_names.R
+++ R/extract_drug_names.R
@@ -1,4 +1,6 @@
-#' Extract drug name
+#' Extract drug names
+#'
+#' Extracts named drug names from the intervention
 #'
 #' @param intervention A character string containing an unprocessed
 #'   intervention name (e.g. "Aspirin 150 mg" will return "Aspirin")
@@ -23,20 +25,26 @@ extract_drug_names <- function(intervention) {
     remove_before_matching <- c(
       "high dose",
       "low dose",
+      "single dose",
+      "intermediate dose",
       "fixed repeated dose",
       "fixed dose",
       "repeated dose",
       "Pharmacological",
       "Laboratory",
       "Biomarker",
+      "diagnostic",
       "Protocol",
       "drug",
       "drugs",
       "pressurized",
       "tablets",
+      "liposome injection",
+      "injectable",
       "Injection",
       "Injections",
       "Inj",
+      "platinum agent",
       "Agent",
       "Chemotherapy",
       "Therapy",
@@ -44,13 +52,14 @@ extract_drug_names <- function(intervention) {
       "Assessment",
       "Analysis",
       "Hormonal",
-      "stem cell",
+      "stem cell(|s)",
       "autologous",
       "allogeneic",
       "transplantation",
       "hypomethylating",
       "akt inhibitor",
       "natural killer",
+      "[A-Za-z0-9-\\s]+\\st(\\s|-)cell(|s)",
       "cell",
       "surgery",
       "for tumor removal",
@@ -63,14 +72,53 @@ extract_drug_names <- function(intervention) {
       "intravenous solution",
       "solution",
       "monotherapy",
+      "combination of",
       "combination",
+      "radiotherapy",
+      "intensity",
       "recommended phase 2 dose",
       "oral capsule",
       "oral",
-      "capsule",
+      "capsule(|s)",
       "alone",
       "without",
-      "formerly"
+      "formerly",
+      "monoclonal",
+      "antibody",
+      "modified",
+      "radiation",
+      "fractionated",
+      "induction",
+      "maintenance",
+      "modulated",
+      "herbal",
+      "decoction",
+      "investigator's choice",
+      "contrast-enhanced magnetic resonance imaging",
+      "magnetic resonance imaging",
+      "imaging",
+      "administration",
+      "intrapleural",
+      "in situ hybridization",
+      "chromogenic",
+      "fluorescence",
+      "gene expression",
+      "engineered",
+      "transplant",
+      "increasing",
+      "suspension",
+      "nanoparticles",
+      "frozen",
+      "liquid",
+      "lyophilized powder",
+      "powder",
+      "matching placebo",
+      "placebo",
+      "quality-of-life",
+      "quality of life",
+      "induction",
+      "phase [0-9]",
+      "phase [i]+"
     )
     
     ## Remove dosage, formulation details, cohort numbers, and
@@ -78,16 +126,21 @@ extract_drug_names <- function(intervention) {
     intervention <- stringr::str_replace_all(
       intervention,
       stringr::regex(
-        "\\b\\d+(\\.?\\d*)?\\s?(mg/kg|mg/ml|mg|ml|Tablet|Tablets)\\b",
+        "\\b\\d+(\\.?\\d*)?\\s?(mg/kg|mg/ml|mg/m2|mg|ml|Tablet|Tablets)\\b",
         ignore_case = TRUE
       ), " ") %>%
       stringr::str_replace_all("\\bCohort\\s*[A-Za-z0-9]+\\b", "") %>%
-      stringr::str_replace_all("\\bArm\\s*\\d*\\b", "") %>%
+      stringr::str_replace_all("\\bArm\\s*[A-Za-z0-9]+\\b", "") %>%
       stringr::str_replace_all("\\s[0-9]+%\\s", "") %>%
       stringr::str_replace_all("\\([0-9]+%\\s", "(") %>%
       stringr::str_replace_all("\\s[0-9]+%\\)", ")") %>%
       stringr::str_replace_all("(\\s|\\(|\\))[0-9]+%(\\s|\\(|\\))", "") %>%
-      stringr::str_replace_all("\\b\\d* Day Cycle\\b", "")
+      stringr::str_replace_all("\\b\\d* Day Cycle\\b", "") %>%
+      stringr::str_replace_all(stringr::regex("\\banti-[A-Za-z0-9-]+\\b", ignore_case = TRUE), "") %>%
+      stringr::str_replace_all("\\b[A-Za-z0-9-]+(\\s?|-)(t|T)argeted (c|C)heckpoint (i|I)nhibitor\\b", "") %>%
+      stringr::str_replace_all("\\b[A-Za-z0-9-]+(\\s?|-)(t|Targeted) (i|I)nhibitor\\b", "") %>%
+      stringr::str_replace_all("\\b[A-Za-z0-9-]+(\\s?|-)(i|I)nhibitor\\b", "") %>%
+      stringr::str_replace_all("\\b[A-Za-z0-9-]+(\\s?|-)(s|S)pecific\\b", "")
 
     for (i in 1:length(remove_before_matching)) {
       intervention <- stringr::str_replace_all(
@@ -110,7 +163,7 @@ extract_drug_names <- function(intervention) {
     intervention <- stringr::str_replace_all(
       intervention,
       stringr::regex(
-        "\\b(and|or|plus|with|in)\\b|[+]",
+        "\\b(and|or|plus|combined with|with|in)\\b|[+]",
         ignore_case = TRUE
       ),
       ","
@@ -159,16 +212,29 @@ extract_drug_names <- function(intervention) {
       "OncoSec Medical System",
       "OMS",
       "Monotherapy",
-      "of two marketed",
+      "two marketed",
       "PIPAC",
       "IntraPeritoneal Air-flow",
       "cutaneous",
-      "ocular"
+      "ocular",
+      "Electro-Hyperthermia",
+      "mEHT",
+      "TCM",
+      "of",
+      "EBV-CTLs",
+      "MRI",
+      "an",
+      "ASCT",
+      "for",
+      "t cells"
     )
 
     ## Remove the `exclude terms`
     clean_matches <- matches[! tolower(matches) %in% tolower(exclude_terms)]
 
+    ## Remove anything that's a single character
+    clean_matches <- clean_matches[nchar(clean_matches) > 1]
+    
     ## Return matches if any, otherwise NA
     if (length(clean_matches) > 0) {
       return(clean_matches)
