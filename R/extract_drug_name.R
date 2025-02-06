1178908c63b47d8226a8f70d39ad94635909c410
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2025-02-06 09:49:23 -0500

    Rework function with better logic and unit testing

--- R/extract_drug_name.R
+++ R/extract_drug_name.R
@@ -12,128 +12,168 @@
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 
-extract_drug_name <- function(intervention) {
+extract_drug_names <- function(intervention) {
 
   out <- tryCatch({
 
-    extraction <- c(
-      "^drug: ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+) and ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+) or ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+) plus ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+) plus modified ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+) + ([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+)+([A-Za-z0-9-]+)$",
-      "^([A-Za-z0-9-]+)(?:, ([A-Za-z0-9-]+))*$",
-      "^([A-Za-z0-9-]+)$"
-    )
+    ## The pattern that any drug name has to match (includes letters,
+    ## numbers, hyphens, periods and spaces)
+    drug_regex <- "\\b[A-Za-z0-9-\\.]+(?:\\s[A-Za-z0-9-\\.]+)*\\b"
 
-    extraction <- tibble::tribble(
-      ~match,
-      ~extract,
-
-      ## Matches "sunitinib malate"
-      "^([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+)$",
-
-      ## Matches "Drug: sunitinib malate"
-      "^drug: ([A-Za-z0-9 -]+)$",
-      "^drug: ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate (drug)"
-      "^([A-Za-z0-9 -]+) \\(drug\\)$",
-      "^([A-Za-z0-9 -]+) \\(drug\\)$",
-
-      ## Matches "sunitinib malate and sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) and ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) and ([A-Za-z0-9 -]+)$",
-      
-      ## Matches "sunitinib malate or sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) or ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) or ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate plus sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) plus ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) plus ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate plus modified sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) plus modified ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) plus modified ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate with sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) with ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) with ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate + sorafenib tosylate"
-      "^([A-Za-z0-9 -]+) \\+ ([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+) \\+ ([A-Za-z0-9 -]+)$",
-
-      ## Matches "sunitinib malate+sorafenib tosylate"
-      "^([A-Za-z0-9 -]+)\\+([A-Za-z0-9 -]+)$",
-      "^([A-Za-z0-9 -]+)\\+([A-Za-z0-9 -]+)$",
-
-      ## Matches "300 mg sunitinib malate"
-      "^[0-9 ]+mg ([A-Za-z0-9 -]+)$",
-      "^[0-9 ]+mg ([A-Za-z0-9 -]+)$"
-      ## Matches "sunitinib malate 300 mg"
-      ## Matches "sunitinib malate 300 mg tablet"
-      ## Matches "sunitinib malate, sorafenib tosylate, eribulin
-      ## mesylate"
-      
-    )
-
-    extraction <- extraction %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(found = grepl(.data$match, intervention)) %>%
-      dplyr::ungroup() %>%
-      dplyr::filter(found) %>%
-      dplyr::pull(extract)
-
-    ## Pull out matches
-    extracted <- stringr::str_match(
-      intervention,
-      stringr::regex(extraction, ignore_case = TRUE)
-    )
-
-    ## Collapse the matrix into a list
-    extracted <- c(extracted)
-
-    ## Remove duplicates
-    extracted <- unique(extracted)
-
-    ## Remove NA's
-    extracted <- extracted[! is.na(extracted)]
-
-    ## Remove the original string
-    extracted <- extracted[extracted != intervention]
-
-
-    ## Failure strings: If the intervention matches the following
-    ## strings, return NA
-    failure <- c(
-      "investigational agent",
-      "chemotherapy",
-      "standard of care",
-      "physician's choice",
-      "questionnaire",
-      "injection",
-      "treatment",
-      "imaging",
+    remove_before_matching <- c(
       "high dose",
       "low dose",
-      "biopsy",
-      "laboratory biomarker analysis",
-      "peripheral stem cell collection",
-      "capsules"
+      "fixed repeated dose",
+      "fixed dose",
+      "repeated dose",
+      "Pharmacological",
+      "Laboratory",
+      "Biomarker",
+      "Protocol",
+      "drug",
+      "drugs",
+      "pressurized",
+      "tablets",
+      "Injection",
+      "Injections",
+      "Inj",
+      "Agent",
+      "Chemotherapy",
+      "Therapy",
+      "Study",
+      "Assessment",
+      "Analysis",
+      "Hormonal",
+      "stem cell",
+      "autologous",
+      "allogeneic",
+      "transplantation",
+      "hypomethylating",
+      "akt inhibitor",
+      "natural killer",
+      "cell",
+      "surgery",
+      "for tumor removal",
+      "resection",
+      "hsp90 inhibitor",
+      "inhibitor",
+      "pathway",
+      "signalling",
+      "notch",
+      "intravenous solution",
+      "solution",
+      "monotherapy",
+      "combination",
+      "recommended phase 2 dose",
+      "oral capsule",
+      "oral",
+      "capsule",
+      "alone",
+      "without",
+      "formerly"
     )
     
-    ## Remove failure strings
-    extracted <- extracted[! extracted %in% failure]
+    ## Remove dosage, formulation details, cohort numbers, and
+    ## unwanted descriptors prior to matching
+    intervention <- stringr::str_replace_all(
+      intervention,
+      stringr::regex(
+        "\\b\\d+(\\.?\\d*)?\\s?(mg/kg|mg/ml|mg|ml|Tablet|Tablets)\\b",
+        ignore_case = TRUE
+      ), " ") %>%
+      stringr::str_replace_all("\\bCohort\\s*[A-Za-z0-9]+\\b", "") %>%
+      stringr::str_replace_all("\\bArm\\s*\\d*\\b", "") %>%
+      stringr::str_replace_all("\\s[0-9]+%\\s", "") %>%
+      stringr::str_replace_all("\\([0-9]+%\\s", "(") %>%
+      stringr::str_replace_all("\\s[0-9]+%\\)", ")") %>%
+      stringr::str_replace_all("(\\s|\\(|\\))[0-9]+%(\\s|\\(|\\))", "") %>%
+      stringr::str_replace_all("\\b\\d* Day Cycle\\b", "")
+
+    for (i in 1:length(remove_before_matching)) {
+      intervention <- stringr::str_replace_all(
+        intervention,
+        stringr::regex(
+          paste0(
+            "\\b",
+            remove_before_matching[[i]],
+            "\\b"
+          ),
+          ignore_case = TRUE
+        ),
+        ""
+      )
+    }
     
-    if (length(extracted) > 0) {
-      return(extracted)
+    intervention <- stringr::str_trim(intervention)
+
+    ## Replace conjunctions with a comma
+    intervention <- stringr::str_replace_all(
+      intervention,
+      stringr::regex(
+        "\\b(and|or|plus|with|in)\\b|[+]",
+        ignore_case = TRUE
+      ),
+      ","
+    )
+
+    ## Split string into a list using commas
+    matches <- unlist(stringr::str_split(intervention, ",")) %>%
+      trimws() %>%
+      stringr::str_extract_all(drug_regex) %>%
+      unlist()
+    
+    ## Words to remove after matching
+    exclude_terms <- c(
+      "Injection",
+      "Cohort",
+      "Analysis",
+      "Therapy",
+      "Study",
+      "Assessment",
+      "Placebo",
+      "Questionnaire",
+      "tablets",
+      "Tablet",
+      "Laboratory",
+      "Biomarker",
+      "Pharmacological Study",
+      "External beam radiotherapy",
+      "Intensity-modulated radiotherapy",
+      "IMRT",
+      "Phase I",
+      "Phase 1",
+      "Phase Ib",
+      "Phase 1b",
+      "Phase II",
+      "Phase 2",
+      "Phase III",
+      "Phase 3",
+      "Radiation",
+      "Dendritic",
+      "DC",
+      "Complete",
+      "HMA",
+      "gamma-secretase",
+      "PD-1 targeted checkpoint",
+      "in",
+      "OncoSec Medical System",
+      "OMS",
+      "Monotherapy",
+      "of two marketed",
+      "PIPAC",
+      "IntraPeritoneal Air-flow",
+      "cutaneous",
+      "ocular"
+    )
+
+    ## Remove the `exclude terms`
+    clean_matches <- matches[! tolower(matches) %in% tolower(exclude_terms)]
+
+    ## Return matches if any, otherwise NA
+    if (length(clean_matches) > 0) {
+      return(clean_matches)
     } else {
-      return (NA)
+      return(NA)
     }
 
   },
--- tests/testthat/test-extract_drug_name.R
+++ tests/testthat/test-extract_drug_name.R
@@ -1,17 +1,572 @@
-test_that("Drug extraction algorithm works", {
+test_that(
+  "sunitinib",
+  {
+    expect_equal(
+      extract_drug_names("sunitinib"),
+      "sunitinib"
+    )
+  }
+)
 
-  tibble::tribble(
-    ~intervention, ~intended_result,
+test_that(
+  "Gemcitabine and Cisplatin",
+  {
+    expect_equal(
+      extract_drug_names("Gemcitabine and Cisplatin"),
+      c("Gemcitabine", "Cisplatin")
+    )
+  }
+)
 
-    "Aspirin",
-    c("Aspirin"),
+test_that(
+  "Donafenib 200mg",
+  {
+    expect_equal(
+      extract_drug_names("Donafenib 200mg"),
+      "Donafenib"
+    )
+  }
+)
 
-    "Aspirin or Tylenol",
-    c("Aspirin", "Tylenol")
-  )
-  
-  expect_equal(
-    1,
-    1
-  )
-})
+test_that(
+  "Erlotinib + Sorafenib",
+  {
+    expect_equal(
+      extract_drug_names("Erlotinib + Sorafenib"),
+      c("Erlotinib", "Sorafenib")
+    )
+  }
+)
+
+
+
+test_that(
+  "Nab-paclitaxel and S-1",
+  {
+    expect_equal(
+      extract_drug_names("Nab-paclitaxel and S-1"),
+      c("Nab-paclitaxel", "S-1")
+    )
+  }
+)
+
+test_that(
+  "Laboratory Biomarker Analysis",
+  {
+    expect_equal(
+      extract_drug_names("Laboratory Biomarker Analysis"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Nivolumab 40 mg in 4 ml Injection",
+  {
+    expect_equal(
+      extract_drug_names("Nivolumab 40 mg in 4 ml Injection"),
+      "Nivolumab"
+    )
+  }
+)
+
+test_that(
+  "Atezolizumab Cohort 7",
+  {
+    expect_equal(
+      extract_drug_names("Atezolizumab Cohort 7"),
+      "Atezolizumab"
+    )
+  }
+)
+
+test_that(
+  "Atezolizumab Cohort 7",
+  {
+    expect_equal(
+      extract_drug_names("Interferon Gamma-1b"),
+      "Interferon Gamma-1b"
+    )
+  }
+)
+
+test_that(
+  "Nivolumab/Ipilimumab",
+  {
+    expect_equal(
+      extract_drug_names("Nivolumab/Ipilimumab"),
+      c("Nivolumab", "Ipilimumab")
+    )
+  }
+)
+
+test_that(
+  "aspirin and tylenol",
+  {
+    expect_equal(
+      extract_drug_names("aspirin and tylenol"),
+      c("aspirin", "tylenol")
+    )
+  }
+)
+
+test_that(
+  "Dexamethasone tablets",
+  {
+    expect_equal(
+      extract_drug_names("Dexamethasone tablets"),
+      "Dexamethasone"
+    )
+  }
+)
+
+test_that(
+  "Pegylated Interferon alpha-2b",
+  {
+    expect_equal(
+      extract_drug_names("Pegylated Interferon alpha-2b"),
+      "Pegylated Interferon alpha-2b"
+    )
+  }
+)
+
+test_that(
+  "eribulin mesylate + erlotinib",
+  {
+    expect_equal(
+      extract_drug_names("eribulin mesylate + erlotinib"),
+      c("eribulin mesylate", "erlotinib")
+    )
+  }
+)
+
+test_that(
+  "Pharmacological Study",
+  {
+    expect_equal(
+      extract_drug_names("Pharmacological Study"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Bemcentinib; pembrolizumab",
+  {
+    expect_equal(
+      extract_drug_names("Bemcentinib; pembrolizumab"),
+      c("Bemcentinib", "pembrolizumab")
+    )
+  }
+)
+
+test_that(
+  "ADI-PEG 20 plus modified FOLFOX6",
+  {
+    expect_equal(
+      extract_drug_names("ADI-PEG 20 plus modified FOLFOX6"),
+      c("ADI-PEG 20", "modified FOLFOX6")
+    )
+  }
+)
+
+test_that(
+  "Letrozole 2.5Mg Tablet",
+  {
+    expect_equal(
+      extract_drug_names("Letrozole 2.5Mg Tablet"),
+      "Letrozole"
+    )
+  }
+)
+
+test_that(
+  "Hormonal Therapy with Anastrozole and Fulvestrant",
+  {
+    expect_equal(
+      extract_drug_names("Hormonal Therapy with Anastrozole and Fulvestrant"),
+      c("Anastrozole", "Fulvestrant")
+    )
+  }
+)
+
+test_that(
+  "Docetaxel, nedaplatin, fluorouracil",
+  {
+    expect_equal(
+      extract_drug_names("Docetaxel, nedaplatin, fluorouracil"),
+      c("Docetaxel", "nedaplatin", "fluorouracil")
+    )
+  }
+)
+
+test_that(
+  "autologous or allogeneic stem cell transplantation",
+  {
+    expect_equal(
+      extract_drug_names("autologous or allogeneic stem cell transplantation"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Intensity-modulated radiotherapy (IMRT)",
+  {
+    expect_equal(
+      extract_drug_names("Intensity-modulated radiotherapy (IMRT)"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Trastuzumab, TS ONE, Cisplatin",
+  {
+    expect_equal(
+      extract_drug_names("Trastuzumab, TS ONE, Cisplatin"),
+      c("Trastuzumab", "TS ONE", "Cisplatin")
+    )
+  }
+)
+
+test_that(
+  "MEDI-551 4 mg/kg",
+  {
+    expect_equal(
+      extract_drug_names("MEDI-551 4 mg/kg"),
+      "MEDI-551"
+    )
+  }
+)
+
+test_that(
+  "Akt inhibitor MK2206",
+  {
+    expect_equal(
+      extract_drug_names("Akt inhibitor MK2206"),
+      "MK2206"
+    )
+  }
+)
+
+test_that(
+  "Complete Resection - Surgery for tumor removal",
+  {
+    expect_equal(
+      extract_drug_names("Complete Resection - Surgery for tumor removal"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Dendritic Cell (DC) Injections",
+  {
+    expect_equal(
+      extract_drug_names("Dendritic Cell (DC) Injections"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Radiation Therapy",
+  {
+    expect_equal(
+      extract_drug_names("Radiation Therapy"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Phase I: Glembatumumab Vedotin",
+  {
+    expect_equal(
+      extract_drug_names("Phase I: Glembatumumab Vedotin"),
+      "Glembatumumab Vedotin"
+    )
+  }
+)
+
+test_that(
+  "E7389 28 Day Cycle",
+  {
+    expect_equal(
+      extract_drug_names("E7389 28 Day Cycle"),
+      "E7389"
+    )
+  }
+)
+
+test_that(
+  "FOLFOXIRI Protocol",
+  {
+    expect_equal(
+      extract_drug_names("FOLFOXIRI Protocol"),
+      "FOLFOXIRI"
+    )
+  }
+)
+
+test_that(
+  "Hypomethylating Agent (HMA)",
+  {
+    expect_equal(
+      extract_drug_names("Hypomethylating Agent (HMA)"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "gamma-secretase/Notch signalling pathway inhibitor RO4929097",
+  {
+    expect_equal(
+      extract_drug_names("gamma-secretase/Notch signalling pathway inhibitor RO4929097"),
+      "RO4929097"
+    )
+  }
+)
+
+test_that(
+  "gamma-secretase/Notch signalling pathway inhibitor RO4929097",
+  {
+    expect_equal(
+      extract_drug_names("gamma-secretase/Notch signalling pathway inhibitor RO4929097"),
+      "RO4929097"
+    )
+  }
+)
+
+test_that(
+  "PV-10 (10% rose bengal disodium)",
+  {
+    expect_equal(
+      extract_drug_names("PV-10 (10% rose bengal disodium)"),
+      c("PV-10", "rose bengal disodium")
+    )
+  }
+)
+
+test_that(
+  "glembatumumab vedotin and PD-1 targeted checkpoint inhibitor (nivolumab OR pembrolizumab)",
+  {
+    expect_equal(
+      extract_drug_names("glembatumumab vedotin and PD-1 targeted checkpoint inhibitor (nivolumab OR pembrolizumab)"),
+      c("glembatumumab vedotin", "nivolumab", "pembrolizumab")
+    )
+  }
+)
+
+test_that(
+  "Panitumumab 20 MG/ML Intravenous Solution [VECTIBIX]",
+  {
+    expect_equal(
+      extract_drug_names("Panitumumab 20 MG/ML Intravenous Solution [VECTIBIX]"),
+      c("Panitumumab", "VECTIBIX")
+    )
+  }
+)
+
+test_that(
+  "OncoSec Medical System (OMS)",
+  {
+    expect_equal(
+      extract_drug_names("OncoSec Medical System (OMS)"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "ON 01910.Na",
+  {
+    expect_equal(
+      extract_drug_names("ON 01910.Na"),
+      "ON 01910.Na"
+    )
+  }
+)
+
+test_that(
+  "INCB040093 Monotherapy",
+  {
+    expect_equal(
+      extract_drug_names("INCB040093 Monotherapy"),
+      "INCB040093"
+    )
+  }
+)
+
+test_that(
+  "Mocetinostat - 50 mg",
+  {
+    expect_equal(
+      extract_drug_names("Mocetinostat - 50 mg"),
+      "Mocetinostat"
+    )
+  }
+)
+
+test_that(
+  "Mocetinostat - Recommended Phase 2 Dose (70 mg)",
+  {
+    expect_equal(
+      extract_drug_names("Mocetinostat - Recommended Phase 2 Dose (70 mg)"),
+      "Mocetinostat"
+    )
+  }
+)
+
+test_that(
+  "Arm 1: L19IL2 + Dacarbazine",
+  {
+    expect_equal(
+      extract_drug_names("Arm 1: L19IL2 + Dacarbazine"),
+      c("L19IL2", "Dacarbazine")
+    )
+  }
+)
+
+test_that(
+  "Combination of two marketed drugs (irinotecan and cisplatin)",
+  {
+    expect_equal(
+      extract_drug_names("Combination of two marketed drugs (irinotecan and cisplatin)"),
+      c("irinotecan", "cisplatin")
+    )
+  }
+)
+
+test_that(
+  "Pressurized IntraPeritoneal Air-flow Chemotherapy (PIPAC) fixed repeated dose",
+  {
+    expect_equal(
+      extract_drug_names("Pressurized IntraPeritoneal Air-flow Chemotherapy (PIPAC) fixed repeated dose"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "Bortezomib (drug)",
+  {
+    expect_equal(
+      extract_drug_names("Bortezomib (drug)"),
+      "Bortezomib"
+    )
+  }
+)
+
+test_that(
+  "Vancomycin Oral Capsule",
+  {
+    expect_equal(
+      extract_drug_names("Vancomycin Oral Capsule"),
+      "Vancomycin"
+    )
+  }
+)
+
+test_that(
+  "MEDI4736 in combination with nab-paclitaxel and gemcitabine",
+  {
+    expect_equal(
+      extract_drug_names("MEDI4736 in combination with nab-paclitaxel and gemcitabine"),
+      c("MEDI4736", "nab-paclitaxel", "gemcitabine")
+    )
+  }
+)
+
+test_that(
+  "'SMT-NK' Inj (allogeneic Natural Killer cell)",
+  {
+    expect_equal(
+      extract_drug_names("'SMT-NK' Inj (allogeneic Natural Killer cell)"),
+      "SMT-NK"
+    )
+  }
+)
+
+test_that(
+  "Nivolumab: Cohort 1 (Cutaneous)",
+  {
+    expect_equal(
+      extract_drug_names("Nivolumab: Cohort 1 (Cutaneous)"),
+      "Nivolumab"
+    )
+  }
+)
+
+test_that(
+  "Phase Ib: Fisogatinib (BLU-554) 400mg in combination with Sugemalimab (CS1001) 1200mg",
+  {
+    expect_equal(
+      extract_drug_names("Phase Ib: Fisogatinib (BLU-554) 400mg in combination with Sugemalimab (CS1001) 1200mg"),
+      c("Fisogatinib", "BLU-554", "Sugemalimab", "CS1001")
+    )
+  }
+)
+
+test_that(
+  "Cohort A: varlilumab & ipilimumab; Cohort B: varlilumab, ipilimumab, CDX-1401 & poly-ICLC",
+  {
+    expect_equal(
+      extract_drug_names("Cohort A: varlilumab & ipilimumab; Cohort B: varlilumab, ipilimumab, CDX-1401 & poly-ICLC"),
+      c("varlilumab", "ipilimumab", "varlilumab", "ipilimumab", "CDX-1401", "poly-ICLC")
+    )
+  }
+)
+
+test_that(
+  "Oral 5-Azacitidine 200 MG",
+  {
+    expect_equal(
+      extract_drug_names("Oral 5-Azacitidine 200 MG"),
+      "5-Azacitidine"
+    )
+  }
+)
+
+test_that(
+  "Investigator's choice of platinum agent",
+  {
+    expect_equal(
+      extract_drug_names("Investigator's choice of platinum agent"),
+      NA
+    )
+  }
+)
+
+test_that(
+  "GL-ONC1 alone, or in combination with chemotherapy with or without bevacizumab",
+  {
+    expect_equal(
+      extract_drug_names("GL-ONC1 alone, or in combination with chemotherapy with or without bevacizumab"),
+      c("GL-ONC1", "bevacizumab")
+    )
+  }
+)
+
+test_that(
+  "Hsp90 inhibitor AUY922",
+  {
+    expect_equal(
+      extract_drug_names("Hsp90 inhibitor AUY922"),
+      "AUY922"
+    )
+  }
+)
+
+test_that(
+  "GSK1363089 (formerly XL880)",
+  {
+    expect_equal(
+      extract_drug_names("GSK1363089 (formerly XL880)"),
+      c("GSK1363089", "XL880")
+    )
+  }
+)
+
